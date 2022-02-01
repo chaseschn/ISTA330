5a0b5e8eb60c981e0dbb642fad5364885de867bb
chaseschn <77998082+chaseschn@users.noreply.github.com>
2022-01-31 21:06:10 -0700

    Update question6.js

--- practicum/week-2/question6.js
+++ practicum/week-2/question6.js
@@ -5,16 +5,16 @@ input: [1, 2, 2, 3, 4, 7, 9]
 output: 3 
 */
 
-function funct(n){
+var median = function(input) {
   let mid = n.sort().length;
   let midsort= n.sort();
   if (mid%2 == 0){
- var  mid1 = mid/2;
-  ar = (midsort[mid1] + midsort[mid1-1])/2;
-  return ar;
-      }
-    else{
-      var mid1 = Math.floor(mid/2)
-      return midsort[mid1];
-    }
-      }
+    var  mid1 = mid/2;
+    ar = (midsort[mid1] + midsort[mid1-1])/2;
+    return ar;
+   }
+   else{
+     var mid1 = Math.floor(mid/2)
+     return midsort[mid1];
+   }
+};
